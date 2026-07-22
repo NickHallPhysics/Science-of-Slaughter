232dd92e1978774e3f89699852c81a31f8803f12
NickHallPhysics <nicholas-h@hotmail.co.uk>
2026-07-22 11:37:53 +0100

    Added GitHub page to vite.config to allow hosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://nickhallphysics.github.io/Science-of-Slaughter/",
   test: {
     globals: false,
     environment: 'node',
